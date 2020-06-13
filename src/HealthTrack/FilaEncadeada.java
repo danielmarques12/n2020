4143aecbbbb74933e09d51906c0b6b2c75cac864
daniel <daniel.brz2009@gmail.com>
2020-06-12 22:42:58 -0300

    Subindo

---
+++ src/HealthTrack/FilaEncadeada.java
@@ -0,0 +1,56 @@
+package HealthTrack;
+
+public class FilaEncadeada {
+
+    private class No{
+        Paciente paciente;
+        No proximo;
+    }
+
+    private No inicio;
+    private No fim;
+
+    public void init(){
+        inicio = fim = null;
+    }
+
+    public boolean isEmpty(){
+
+        if(inicio == null && fim == null)
+            return true;
+        else
+            return false;
+    }
+
+    public void enqueue(Paciente elemento){
+
+        No novo = new No();
+
+        novo.paciente = elemento;
+        novo.proximo = null;
+
+        if(isEmpty())
+            inicio = novo;
+        else
+            fim.proximo = novo;
+
+        fim = novo;
+    }
+
+    public Paciente dequeue(){
+
+        Paciente valor = inicio.paciente;
+        inicio = inicio.proximo;
+
+        if(inicio == null){
+            fim = null;
+        }
+
+        return valor;
+    }
+
+    public Paciente first(){
+        return inicio.paciente;
+    }
+
+}
